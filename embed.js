3b858fe84270edfa8b25e6a9972da0788199ccdf
ghigbie <gehigbie@gmail.com>
2017-03-16 21:34:58 +0000

    added new user for test

--- embed.js
+++ embed.js
@@ -2,7 +2,22 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/bog_demo2");
 
 //define User model - email and name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String
+});
 
+var User = mongoose.model("User", userSchema);
 
 //define POST model - title, content
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
 
+var Post = mongoose.model("Post", postSchema);
+
+var newUSer = new User({
+   email: "charlie@brown.edu",
+   name: "Charlie Brown"
+});
